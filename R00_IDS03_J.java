0404202cd2446e9456657b00fba116130d0c1835
rytothean <161386778+rytothean@users.noreply.github.com>
2024-03-02 22:03:14 -0500

    Add files via upload

---
+++ R00_IDS03_J.java
@@ -0,0 +1,7 @@
+public class Main {
+  if (loginSuccessful) {
+    logger.severe("User login succeeded for: " + username);
+  } else {
+    logger.severe("User login failed for: " + username);
+  }
+}
