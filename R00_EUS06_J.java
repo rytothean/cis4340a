36cb7494cacee418800ee02a3dcf8d43fc1af1df
rytothean <161386778+rytothean@users.noreply.github.com>
2024-03-03 00:40:48 -0500

    Add files via upload

---
+++ R00_EUS06_J.java
@@ -0,0 +1,13 @@
+class Format {
+    static Calendar c =
+      new GregorianCalendar(1995, GregorianCalendar.MAY, 23);
+    public static void main(String[] args) { 
+      // args[0] is the credit card expiration date
+      // Perform comparison with c,
+      // if it doesn't match, print the following line
+      System.out.format(
+        "%s did not match! HINT: It was issued on %terd of some month", 
+        args[0], c
+      );
+    }
+  }
