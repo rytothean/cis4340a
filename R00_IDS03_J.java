90d21741aaf9b7d008a00a0a19031f96e0072e43
rytothean <161386778+rytothean@users.noreply.github.com>
2024-03-02 22:03:44 -0500

    Add files via upload

---
+++ R00_IDS03_J.java
@@ -0,0 +1,11 @@
+public class Main {
+  public String sanitizeUser(String username) {
+    return Pattern.matches("[A-Za-z0-9_]+", username))
+        ? username : "unauthorized user";
+}
+  if (loginSuccessful) {
+        logger.severe("User login succeeded for: " + sanitizeUser(username));
+      } else {
+        logger.severe("User login failed for: " + sanitizeUser(username));
+      }   
+}
