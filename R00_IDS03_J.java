970c25774e593e8563dbc358c19815c796658ec9
rytothean <161386778+rytothean@users.noreply.github.com>
2024-03-02 21:52:37 -0500

    Delete R00_IDS03_J.java

--- R00_IDS03_J.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    if (loginSuccessful) {
-        logger.severe("User login succeeded for: " + sanitizeUser(username));
-      } else {
-        logger.severe("User login failed for: " + sanitizeUser(username));
-      }
-}
